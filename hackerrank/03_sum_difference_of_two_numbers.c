63354da65db2668510a0f09f333792d7d19a4a1c
Sudhakar17 <sudhakarece17@gmail.com>
2020-03-28 18:11:29 +0100

    sum and difference of two numbers

---
+++ hackerrank/03_sum_difference_of_two_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 0, b = 0;
+    float c = 0, d = 0;
+
+    scanf("%d %d",&a,&b);
+    scanf("%f %f",&c,&d);
+
+    // sum and difference of integer and floating numbers
+
+    printf("%d ", a+b);
+    printf("%d\n", a-b);
+    printf("%.1f ", c+d);
+    printf("%.1f\n", c-d);
+    
+    return 0;
+}
+
