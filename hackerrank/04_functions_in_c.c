3207f10586568825c2556ec201c91441fd2d1dcd
Sudhakar17 <sudhakarece17@gmail.com>
2020-03-28 19:57:49 +0100

    find the max of 4 numbers

---
+++ hackerrank/04_functions_in_c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int w, int x, int y, int z)
+{
+    int result, max;
+
+    max = (w > x ? w : x);
+
+    result = (y > z ? (y > max ? y : max):(z > max ? z : max));
+
+    return result;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
