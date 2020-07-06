7f28938ee84cf976983121c6c3bdcd811e7d313e
Sudhakar17 <sudhakarece17@gmail.com>
2020-07-06 17:25:47 +0200

    Added the comments and array subscript way

--- hackerrank/15_dynamic_array.c
+++ hackerrank/15_dynamic_array.c
@@ -1,5 +1,11 @@
 // https://www.hackerrank.com/challenges/dynamic-array-in-c/problem
 
+/* Reference: To understand the dynamic array
+ * https://www.programiz.com/c-programming/c-dynamic-memory-allocation
+ * https://www.techiedelight.com/dynamically-allocate-memory-for-2d-array/
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -39,18 +45,28 @@ int main()
         if (type_of_query == 1) {
             /*
              * Process the query of first type here.
+             * Insert a book with 'y' pages at the end of 'x-th' shelf
              */
             int x, y;
             scanf("%d %d", &x, &y);
             *(total_number_of_books + x) += 1;
             *(total_number_of_pages+x)=realloc(*(total_number_of_pages+x), *(total_number_of_books+x)*sizeof(int));
-            *(*(total_number_of_pages+x)+*(total_number_of_books+x)-1)=y;
+            *(*(total_number_of_pages+x)+*(total_number_of_books+x)-1)=y; // 2D-array
+
+            /* Rewritten the above method with array subscript
+            total_number_of_books[x] += 1;
+            total_number_of_pages[x] = realloc(total_number_of_pages[x],
+            total_number_of_books[x]*sizeof(int));
+            total_number_of_pages[x][total_number_of_books[x]-1] =y;
+            */
 
         } else if (type_of_query == 2) {
+            // Print the number of pages in the y-th book on the x-th shelf
             int x, y;
             scanf("%d %d", &x, &y);
             printf("%d\n", *(*(total_number_of_pages + x) + y));
         } else {
+            // Print the number of books on the x-th shelf
             int x;
             scanf("%d", &x);
             printf("%d\n", *(total_number_of_books + x));
